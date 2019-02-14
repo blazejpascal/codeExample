d1d5992cbb439e7ee28427cb8eda3285dc84310e
Wojciech Twarog <wojciech.twarog@gmail.com>
2019-02-14 10:56:56 +0000

    Added get request, basic Donations component

--- src/components/scenes/Home/Home.tsx
+++ src/components/scenes/Home/Home.tsx
@@ -1,12 +1,53 @@
-import React from 'react'
 
+import React, { Component } from 'react'
+import axios from 'axios'
 
-export default function Donations() {
-  return (
-    <>
-        <h1>last donations</h1>  
-        <div className="donations">
-        </div>
-    </>
-  )
+import Donations from './components/Donations/Donations'
+
+interface IHomeState {
+    fetching: boolean,
+    donations: Array<string>,
+    errors: string
 }
+
+interface IHomeProps {
+
+}
+
+export default class Home extends Component<IHomeProps, IHomeState> {
+    state ={
+        fetching: false,
+        donations: [],
+        errors: ''
+    }
+
+    componentDidMount() {
+        this.fetchDonations()
+    }
+
+    fetchDonations = async () => {
+        this.setState({
+            fetching: !this.state.fetching
+        })
+        try {
+           const response = await axios.get('https://api.justgiving.com/66651531/v1/charity/183560/donations', {})
+                console.log(response.data)
+                this.setState({
+                    fetching: !this.state.fetching,
+                    donations: response.data.donations
+                })
+
+        } catch (error) {
+            this.setState({errors: error})
+        }
+    }   
+
+  render() {
+    return (
+      <div>
+        <Donations />
+      </div>
+    )
+  }
+}
+
---
+++ src/components/scenes/Home/components/Donations/Donations.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Donations.scss'
+
+export default function Donations() {
+  return (
+    <>
+        <h1>last donations</h1>  
+        <div className="donations">
+        </div>
+    </>
+  )
+}
